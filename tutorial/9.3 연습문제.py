69c45b50a8ed6e83be9e8bcf088872394c7a3cd1
leewt1004 <zeuswt1004@gmail.com>
2024-11-06 19:47:18 +0900

    Create 9.3 연습문제.py

---
+++ tutorial/9.3 연습문제.py
@@ -0,0 +1,21 @@
+## 9.3 연습문제
+
+"""
+여러 줄로 된 문자열 사용하기
+다음 소스 코드를 완성하여 실행 결과대로 문자열이 출력되게 만드세요.
+s = ____________________
+________________________
+________________________
+
+print(s)
+
+- 실행결과 -
+Python is a programming language that lets you work quickly
+and
+integrate systems more effectively.
+"""
+s = '''Python is a programming language that lets you work quickly
+and
+integrate systems more effectively.'''
+
+print(s)
