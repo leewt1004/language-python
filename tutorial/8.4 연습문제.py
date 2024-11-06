beaa669b19cbe2f226d6c452570165630716d373
leewt1004 <zeuswt1004@gmail.com>
2024-11-06 19:08:02 +0900

    Create 8.4 연습문제.py

---
+++ tutorial/8.4 연습문제.py
@@ -0,0 +1,18 @@
+## 8.4 연습문제
+"""
+합격 여부 출력하기
+국어, 영어, 수학, 과학 점수가 있을 때 한 과목이라도 50점 미만이면 불합격이라고 정했습니다. 
+다음 소스 코드를 완성하여 합격이면 True, 불합격이면 False가 출력되게 만드세요.
+korean = 92
+english = 47
+mathematic = 86
+science = 81
+
+print(______________________________)
+"""
+korean = 92
+english = 47
+mathematic = 86
+science = 81
+
+print(korean >= 50 and english >= 50 and mathematic >= 50 and science >= 50)
