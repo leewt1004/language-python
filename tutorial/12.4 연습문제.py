1c0caa5d182f313abc94b28f20171390e0032898
leewt1004 <zeuswt1004@gmail.com>
2024-11-08 09:06:45 +0900

    Create 12.4 연습문제.py

---
+++ tutorial/12.4 연습문제.py
@@ -0,0 +1,40 @@
+## 12.4 연습문제
+"""
+딕셔너리에 게임 캐릭터 능력치 저장하기
+다음 소스 코드를 완성하여 게임 캐릭터의 체력(health)과 이동 속도(movement speed)가 출력되게 만드세요.
+
+camille = {
+    'health': 575.6,
+    'health_regen': 1.7,
+    'mana': 338.8,
+    'mana_regen': 1.63,
+    'melee': 125,
+    'attack_damage': 60,
+    'attack_speed': 0.625,
+    'armor': 26,
+    'magic_resistance': 32.1,
+    'movement_speed': 340
+}
+
+print(__________)
+print(__________)
+
+- 실행 결과 - 
+575.6
+340
+"""
+camille = {
+    'health': 575.6,
+    'health_regen': 1.7,
+    'mana': 338.8,
+    'mana_regen': 1.63,
+    'melee': 125,
+    'attack_damage': 60,
+    'attack_speed': 0.625,
+    'armor': 26,
+    'magic_resistance': 32.1,
+    'movement_speed': 340
+}
+
+print(camille['health'])
+print(camille['movement_speed'])
