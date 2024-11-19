c01045fc83bc76a371942d561d899dc848409973
leewt1004 <zeuswt1004@gmail.com>
2024-11-19 21:17:49 +0900

    Update 34.2 속성 사용하기.py

--- tutorial/34.2 속성 사용하기.py
+++ tutorial/34.2 속성 사용하기.py
@@ -6,4 +6,21 @@ class 클래스이름:
     def __init__(self):
         self.속성 = 값
 """
-class Person
+class Person:
+    def __init__(self) :
+        self.hello = '안녕하세요'   # 속성 정의 및 값 할당
+
+    def greeting(self) :
+        print(self.hello)   # self.hello 속성 값 출력
+
+james = Person()    # 클래스 Person의 인스턴스(객체) james 생성
+print(james.greeting()) # greeting 메서드 호출
+"""
+----- 코드 흐름 및 설명 -----
+
+(1) class Person:
+    - Person 클래스 정의
+
+(2)  def __init__(self) :
+    - 
+"""
