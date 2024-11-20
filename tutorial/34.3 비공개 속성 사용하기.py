7abc92d29c63e1545cccacc569396cdc1b385eb2
leewt1004 <zeuswt1004@gmail.com>
2024-11-20 17:27:51 +0900

    Create 34.3 비공개 속성 사용하기.py

---
+++ tutorial/34.3 비공개 속성 사용하기.py
@@ -0,0 +1,10 @@
+## 34.3 비공개 속성 사용하기
+"""
+앞에서 만든 Person 클래스에는 hello, name, age, address 속성이 있었습니다.
+"""
+class Person :
+    def __init__(self, name, age, addrss) :
+        self.hello = '안녕하세요'
+        self.name = name
+        self.age = age
+        self.address = addrssh
